bc648b469849c6ec20158220ed88c790992f6848
mehdi227 <185486319+mehdi227@users.noreply.github.com>
2025-09-24 15:03:29 +0200

    ...

---
+++ OpdrachtBedrijven/OpdrachtBedrijven CL/DL/BestandLezer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpdrachtBedrijven_CL.DL
+{
+    public class BestandLezer
+    {
+        public BestandLezer() {
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\mehdi\\Downloads\\bedrijvenbelgie_18092025.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
