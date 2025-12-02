43c0050992abc1e10e2edd21292cc078e15f781d
mehdi227 <185486319+mehdi227@users.noreply.github.com>
2025-12-02 11:45:08 +0100

    managers en interfaces toegevoegd

---
+++ SolutionOrder/Order_BL/Managers/OrganisationManager.cs
@@ -0,0 +1,12 @@
+﻿using Order_BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_BL.Managers {
+    public class OrganisationManager : IOrganisationLibrary {
+
+    }
+}
